96f6abd17ac8657838909bbc22f405716f70ac1e
Mohamed Assem <mohamedassem663@gmail.com>
2025-07-13 15:30:38 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DrinksApp());
+}
+
+class DrinksApp extends StatelessWidget {
+  const DrinksApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
